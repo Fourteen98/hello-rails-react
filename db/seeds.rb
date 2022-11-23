b1f9d8ddaf638542591896f0c8cfb8e26e13b58c
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-23 07:44:57 +0000

    add some seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.destroy_all
+Message.create(message: "Hello World!")
+Message.create(message: "Give me a job!")
+Message.create(message: "I'm a good Man!")
+Message.create(message: "Some people are just too good for this world!")
+Message.create(message: "I'm not one of them!")
+p "Created #{Message.count} messages"
